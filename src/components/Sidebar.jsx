9e6903e248105e726986e4d8d2adcacb1015a88d
bobym177 <boby.s@kiranatama.com>
2023-06-05 16:09:42 +0700

    Setup dark mode and light mode

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,7 +19,7 @@ import {
 } from "@mui/material";
 import React from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ mode, setMode }) => {
   return (
     <Box
       flex={1}
@@ -94,7 +94,9 @@ const Sidebar = () => {
               <ListItemIcon>
                 <ModeNightSharp />
               </ListItemIcon>
-              <Switch />
+              <Switch
+                onChange={(e) => setMode(mode === "light" ? "dark" : "light")}
+              />
             </ListItemButton>
           </ListItem>
         </List>
