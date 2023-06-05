41bf37ba4318e7640b23c55ec6b418995b646c38
bobym177 <boby.s@kiranatama.com>
2023-06-05 10:41:38 +0700

    create new layout components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Stack } from "@mui/material";
+
 function App() {
-  return <div></div>;
+  return (
+    <Box>
+      {/* Navbar */}
+      <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
